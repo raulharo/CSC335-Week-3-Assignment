68a857fe89f25e2f66a3f4fa7e5bed2cc916f3d8
Raul Haro <39468886+raulharo@users.noreply.github.com>
2022-11-08 23:20:57 -0500

    Add files via upload

---
+++ Horse.java
@@ -0,0 +1,31 @@
+public class Horse
+{
+   // Define the Horse class here
+   String name;
+   String color;
+   int birthYear;
+
+   public String getName(){
+       return name;
+   }
+
+   public String getColor(){
+       return color;
+   }
+
+   public int getBirthYear(){
+       return birthYear;
+   }
+
+   public void setName(String param){
+       name = param;
+   }
+
+   public void setColor(String param){
+       color = param;
+   }
+
+   public void setBirthYear(int param){
+       birthYear = param;
+   }
+}
---
+++ RaceHorse.java
@@ -0,0 +1,13 @@
+public class RaceHorse extends Horse
+{
+    // Define the RaceHorse class here
+    int races;
+
+    public int getRaces(){
+        return races;
+    }
+
+    public void setRaces(int param){
+        races = param;
+    }
+}
